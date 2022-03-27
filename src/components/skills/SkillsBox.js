1ec9e5a9239d90f241c9f52c5eebf20d653a15c7
Alex <somersault97@gmail.com>
2022-03-27 14:19:06 +0300

    Александра сделала блок Skills

--- src/components/skills/SkillsBox.js
+++ src/components/skills/SkillsBox.js
@@ -1,17 +1,26 @@
 import './SkillsBox.css'
 function SkillsBox() {
     return (
-        <div className="skills-box">
-            <h1 className="skills_heading">Skills:</h1>
-            <div className="skills__inner">
-                <div className="skills__line-box">
-                </div>
-                <div className="skills__line-box">
-                </div>
-                <div className="skills__line-box">
-                </div>
-            </div>
-        </div>
+        <div className="skills"> 
+		<h1 className="heading skills">SKILLS</h1>
+			<div className="box">
+				<div className="skills_inner">
+					<div className="skill_title">Linux</div>
+					<div className="skill_text">автоматизация, виртуализация, защита</div>
+					<div className="skill_year">15 лет</div>
+				</div>
+				<div className="skills_inner">
+					<div className="skill_title">DevOps</div>
+					<div className="skill_text">CI/CD, Kubernetes, Terraform, Ansible</div>
+					<div className="skill_year">3 года</div>
+				</div>
+				<div className="skills_inner">
+					<div className="skill_title">Security</div>
+					<div className="skill_text">информационная и кибер - безопасность</div>
+					<div className="skill_year">5 лет</div>
+				</div>
+			</div>
+		</div>
     )
 }
 export default SkillsBox;
