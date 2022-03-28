0638764c12f12d3ce51e08e386f95952d5809317
Dmitriy Bondarenko <dabwork1982@gmail.com>
2022-03-28 15:18:31 +0300

    add link, fix name font-size

---
+++ src/components/link_thruth/LinkThruth.js
@@ -0,0 +1,16 @@
+import "./LinkThruth.css";
+function LinkThruth() {
+	return (
+		<>
+			<a
+				className="link"
+				href="https://rogovsky.net/"
+				target="_blank"
+				rel="noreferrer noopener"
+			>
+				{""}
+			</a>
+		</>
+	);
+}
+export default LinkThruth;
